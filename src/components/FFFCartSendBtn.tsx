8fd5ef970472627576f388543fdec6c4be9f2dab
AndyChouCoding <andychouforwk@gmail.com>
2023-04-01 23:58:58 +0800

    ComponetsBtnChange

--- src/components/FFFCartSendBtn.tsx
+++ src/components/FFFCartSendBtn.tsx
@@ -2,7 +2,7 @@ const FFFCartSendBtn: React.FunctionComponent = () => {
     return (
         <>
             <button>
-                <a className="p-2 rounded-full text-[#fff] bg-[#18C5BF]" href="./cart">
+                <a className="p-2 ml-4 rounded-full text-[#fff] bg-[#18C5BF]" href="./cart">
                     Add
                 </a>
             </button>
